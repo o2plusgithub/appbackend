c83066029ec1230bbed9d302f1df6663ef776eec
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-19 19:03:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -79,11 +79,8 @@ app.post('/device_auth', urlencodedParser, function(req, res) {
     //const api_key = "AIzaSyAorkPG4QrJ8S0HPYM9rpdktglqO5UHzM8";
     var signedAttestation = req.body.signedAttestation;
     var decode_token_temp = jwt.decode(signedAttestation); //for recoverying nonce
-    var nonce_temp_buff = new Buffer(decode_token_temp.nonce, "base64");
-    var nonce_temp_string = nonce_temp_buff.toString('ascii');
-    var search_id = { nonce: nonce_temp_string };
+    var search_id = { nonce: decode_token_temp.nonce };
     console.log(decode_token_temp.nonce); 
-    console.log(nonce_temp_string);
     device_details_model.find(search_id, function(err, result) {
         if (!err) {
             var unique_id = result.unique_id;
