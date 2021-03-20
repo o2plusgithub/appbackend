b7af43341f524ff744680d57c5a3f839d2b57787
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-20 06:57:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var device_details_server = new Schema({
 }, {
     collection: 'device_details'
 });
-device_details_server.index({ expireAfterSeconds: 180})
+
 //    expire_at: {type: Date, default: Date.now, expires: 3},
 //    partialFilterExpression: { api_key : String }
 var connect = mongoose.createConnection('mongodb+srv://C6hivgPRCjxKGF9f:yW3c3fc8vpM0ego368z80271RCH@o2plusdatabase.vwl00.mongodb.net/devicedetails?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
