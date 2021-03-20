97e5ca137d326db6c79cac0d15e6d9716f097b17
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-20 05:35:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var device_details_server = new Schema({
     unique_id: String,
     nonce: String,
     api_key: String,
-    expire_at: {type: Date, default: Date.now, expires: 3} 
+    expire_at: {type: Date, default: Date.now, expires: 3, partialFilterExpression: { api_key : "jhfjg" }},
 }, {
     collection: 'device_details'
 });
